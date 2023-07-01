a75476c3b7b23fbfd87f9bbc1bac284a06b558b2
Viktoriya Masyuk <viktoriyamasyuk@yandex.ru>
2023-07-02 00:30:43 +0500

    пр13 исправления обработки ошибок

--- controllers/users.js
+++ controllers/users.js
@@ -17,16 +17,16 @@ module.exports.getUsers = (req, res) => {
 // поиск пользователя по id
 module.exports.getUserId = (req, res) => {
   User.findById(req.params.id)
-    .then((user) => res.status(200).send({
-      name: user.name,
-      about: user.about,
-      avatar: user.avatar,
-      _id: user._id,
-    }))
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: "Пользователь с указанным _id не найден." });
+      }
+      return res.status(200).send(user);
+    })
     .catch((err) => {
       if (err.name === "CastError") {
         return res.status(400).send({
-          message: "Пользователь по указанному _id не найден. ",
+          message: "Указан некоректный id. ",
         });
       }
       return res.status(500).send({ message: "Произошла ошибка" });
@@ -58,7 +58,7 @@ module.exports.createUser = (req, res) => {
 module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { name, about })
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .then((user) => res.status(200).send(user))
     .catch((err) => {
       if (err.name === "ValidationError") {
@@ -74,7 +74,7 @@ module.exports.updateUser = (req, res) => {
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { avatar })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then((user) => res.status(200).send(user))
     .catch((err) => {
       if (err.name === "ValidationError") {
