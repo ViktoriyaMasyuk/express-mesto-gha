702be924ad66f5b8dbbaf9314a8f39f12fea9e94
Viktoriya Masyuk <viktoriyamasyuk@yandex.ru>
2023-07-08 21:52:22 +0500

    добавлена централизованная обработка ошибок

--- app.js
+++ app.js
@@ -38,14 +38,14 @@ app.use((req, res, next) => {
 });
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
-
   res
     .status(statusCode)
     .send({
       message: statusCode === 500
         ? "На сервере произошла ошибка"
-        : message
+        : message,
     });
+  next();
 });
 
 app.listen(PORT, () => {
