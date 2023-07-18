bfcf68f498b58612bddfe70c10e7abe1b0e007c8
Viktoriya Masyuk <viktoriyamasyuk@yandex.ru>
2023-07-18 22:17:03 +0500

    добавлен cors

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
-
 const routes = require('./routes/index');
 const { serverError } = require('./middlewares/serverError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000, BASE_PATH } = process.env;
 const app = express();
@@ -13,8 +14,17 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
+app.use(cors);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.use(requestLogger);
 app.use(routes);
+app.use(errorLogger);
 app.use(errors());
 app.use(serverError);
 
